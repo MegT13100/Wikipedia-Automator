c957e34aaf5ffeca8a1a59023b8f6d168d7b94f5
Sarah Matatov <matatov@siebl-0224-08.ews.illinois.edu>
2021-12-01 13:06:18 -0600

    file processing code added

--- Graph.h
+++ Graph.h
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string>
+#include <sstream>
+
 
 #ifndef _GRAPH_
 #define _GRAPH_
@@ -8,6 +10,19 @@ using namespace std;
 struct Graph {
         bool** adjMatrix;
         int n;
+};
+struct Vertex {
+        Vertex() {
+                name_ = "";
+                index_ = -1;
+        }
+        Vertex(string name, string index) {
+                name_ = name;
+                stringstream i(index);
+                i >> index_;
+        }
+        string name_;
+        int index_;
 };
 
 bool containsEdge(Graph const * const g, int src, int dest);
