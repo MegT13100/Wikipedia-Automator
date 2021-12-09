e5a1b8c369d4946371dd62fd4216afdfaff7e9a7
emilycraw4d <emilycraw4d@gmail.com>
2021-12-09 11:49:50 -0600

    kept working

--- forceDirected.cpp
+++ forceDirected.cpp
@@ -1,4 +1,5 @@
 #include "Graph.h"
+#include "forceDirected.h"
 #include "cs225/PNG.h"
 #include <vector>
 #include <map>
@@ -9,24 +10,26 @@
 
 using namespace std;
 
-map<string, vector<int>> GraphVisualization::initialLayout(Graph g) {
+map<string, pair<int,int>> GraphVisualization::initialLayout(Graph* g) {
     // set up a blank PNG as output
-    cs225::PNG* output = new cs225::PNG(g.getNumV * 5, g.getNumV * 5);
+
+    unsigned int height = g->getNumV() * 5;
+
+    cs225::PNG* output = new cs225::PNG(height, height);
     //make a vertex list 
-    vector<Vertex*> vertices = g.getVertices;
+    vector<Vertex*> vertices = g->getVertices();
    
     // go through the vertices and add them to the map.  
     for (Vertex* v : vertices) {
         if (positions.empty()) {
             vector<int> inputs;
-            int x = rand() % output->width_ - 10;
-            int y = rand() % output->height_ - 10;
-            inputs.push_back(x);
-            inputs.push_back(y);
-            positions.insert({v.name_,inputs; });
+            int x = rand() % output->width() - 10;
+            int y = rand() % output->height() - 10;
+            positions.insert({v->name_, make_pair(x,y)});
+            // positions.insert({v.name_,inputs; });
         } else {
-            vector<int> point = pickPoint(output);
-            positions.insert({v.name_, point}) 
+            pair<int,int> point = pickPoint(output);
+            positions.insert({v->name_, point}); 
 
         }
     }
@@ -35,23 +38,21 @@ map<string, vector<int>> GraphVisualization::initialLayout(Graph g) {
 }
 
 // makes sure the random points don't over lap or get too close. (give 5 px between them)
-vector<int> GraphVisualization::pickPoint(cs225::PNG* output) {
-
-    vector<int> final;
+pair<int,int> GraphVisualization::pickPoint(cs225::PNG* output) {
 
     // set changing seed 
     srand(time(0));
     
     // pick a number but not on the peripheral
-    int x = rand() % output->width_ - 10;
-    int y = rand() % output->height_ - 10;
+    int x = rand() % output->width() - 10;
+    int y = rand() % output->height() - 10;
 
-
-    for (string name : position) {
-        if (position[name][0] == x && position[name][1] == y) {
+    // how to iterate through a map? 
+    for (string name : positions) {
+        if (positions[name].first == x && positions[name].second == y) {
             return pickPoint(output);
         } else {
-            int dist = sqrt(pow(position[name][0] - x, 2) + pow(position[name][1] - y, 2));
+            int dist = sqrt(pow(positions[name].first - x, 2) + pow(positions[name].second - y, 2));
             if (dist < 15) {
                 return pickPoint(output);
             }
@@ -59,24 +60,22 @@ vector<int> GraphVisualization::pickPoint(cs225::PNG* output) {
         }
     }
 
-    final.push_back(x);
-    final.push_back(y);
-    
-    return final;  
+    return make_pair(x,y);  
     
 
 }
 
 
 cs225::PNG* GraphVisualization::drawGraph(map<string, pair<int, int>> layout) {
-    return PNG();
+   
 }
 
 
 
 
-cs225::PNG GraphVisualization::drawForceDirectedGraph(map<string, pair<int, int>>, Graph g, double threshold, int maxIter) {
-    i = 1;
+cs225::PNG GraphVisualization::constructForceDirectedGraph(map<string, pair<int, int>>, Graph* g, double threshold, int maxIter) {
+    unsigned int i = 1;
+    vector<Vertex*> vertices = g->getVertices();
     while (i < maxIter && "forces" > threshold) {
         for (Vertex* v : vertices) {
             // double force on v = "sum of all repulsive forces" + "sum of attractive forces from adjacent vertices"
--- forceDirected.h
+++ forceDirected.h
@@ -11,15 +11,15 @@ class GraphVisualization {
     public:
         
         // outputs the final graph image
-        cs225::PNG drawForceDirectedGraph(map<string, pair<int, int>>, Graph g, double threshold, int maxIter);
+        cs225::PNG constructForceDirectedGraph(map<string, pair<int, int>>, Graph* g, double threshold, int maxIter);
         // sets up a random layout of the graph
-        map<string, pair<int, int>> initialLayout(Graph g);
-        // randomly picks a valid point within the space
-        pair<int, int> pickPoint(cs225::PNG* output);
+        map<string, pair<int,int>> initialLayout(Graph* g);
         
         cs225::PNG* drawGraph(map<string, pair<int, int>> layout);
     private:
-        map<string, pair<int, int>> positions;
+        // randomly picks a valid point within the space
+        pair<int,int> pickPoint(cs225::PNG* output);
+        map<string, pair<int,int>> positions;
 
          
 };
